1f804d8bfd549050345abfc0ce31c6acc465bef3
SunWoo.Kim <sunwoo@gurucompany.co.kr>
2025-03-25 15:31:09 +0900

    project init

---
+++ TextToSpeech/SpeakHandler.swift
@@ -0,0 +1,147 @@
+//
+//  TikitSpeakHandler.swift
+//  TreasureIslandFoundationKit
+//
+//  Created by sunwoo on 11/21/24.
+//
+
+import Foundation
+import AVFoundation
+import AudioToolbox
+
+public class SpeakHandler: NSObject {
+
+    private lazy var speechSynthesizer: AVSpeechSynthesizer = { AVSpeechSynthesizer() }()
+
+    private let stackGroup: String = "TikitSpeakHandler"
+    private var callback: SpeakResult? = nil
+    private var currentSpeakEntity: SpeakEntity? = nil
+
+    override init() {
+        super.init()
+        self.speechSynthesizer.delegate = self
+    }
+
+    public func speak(speakEntity: SpeakEntity) {
+        isDeviceInSilentMode { isMuted in
+            if isMuted {
+                self.callback?.onSpeakStatus(utteranceId: self.currentSpeakEntity?.speakId ?? "", speakStatus: SpeakStatus.muted)
+            } else {
+                if self.speechSynthesizer.isSpeaking || self.speechSynthesizer.isPaused {
+                    self.callback?.onSpeakStatus(utteranceId: self.currentSpeakEntity?.speakId ?? "", speakStatus: SpeakStatus.playing)
+                } else {
+                    let utterance = AVSpeechUtterance(string: speakEntity.speakText)
+                    utterance.voice = AVSpeechSynthesisVoice(language: "ko-KR")
+                    utterance.rate = speakEntity.speechRate
+                    utterance.pitchMultiplier = speakEntity.pitch
+                    self.currentSpeakEntity = speakEntity
+                    self.speechSynthesizer.speak(utterance)
+                }
+            }
+        }
+    }
+
+    // check device silent mode
+    func isDeviceInSilentMode(completion: @escaping (Bool) -> Void) {
+        // ID of a system sound(1057)
+        AudioServicesPlaySystemSoundWithCompletion(0) {
+            // If no sound is heard, assume silent mode
+            completion(AVAudioSession.sharedInstance().outputVolume == 0.0)
+        }
+    }
+
+    public func speakPause() {
+        self.speechSynthesizer.pauseSpeaking(at: AVSpeechBoundary.immediate)
+    }
+
+    public func speakResume() {
+        self.speechSynthesizer.continueSpeaking()
+    }
+
+    public func speakStop() {
+        self.speechSynthesizer.stopSpeaking(at: AVSpeechBoundary.immediate)
+    }
+
+    public func speakDestroy() {
+        self.speakStop()
+        self.currentSpeakEntity = nil
+        self.callback = nil
+    }
+
+    public func speakStatusListener(result: SpeakResult) {
+        self.callback = result
+    }
+}
+
+extension SpeakHandler: AVSpeechSynthesizerDelegate {
+    public func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didStart utterance: AVSpeechUtterance) {
+        callback?.onSpeakStatus(utteranceId: currentSpeakEntity?.speakId ?? "", speakStatus: SpeakStatus.start)
+    }
+
+    public func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        callback?.onSpeakStatus(utteranceId: currentSpeakEntity?.speakId ?? "", speakStatus: SpeakStatus.done)
+    }
+
+    public func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didPause utterance: AVSpeechUtterance) {
+        callback?.onSpeakStatus(utteranceId: currentSpeakEntity?.speakId ?? "", speakStatus: SpeakStatus.pause)
+    }
+
+    public func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didContinue utterance: AVSpeechUtterance) {
+        callback?.onSpeakStatus(utteranceId: currentSpeakEntity?.speakId ?? "", speakStatus: SpeakStatus.resume)
+    }
+
+    public func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didCancel utterance: AVSpeechUtterance) {
+        callback?.onSpeakStatus(utteranceId: currentSpeakEntity?.speakId ?? "", speakStatus: SpeakStatus.stop)
+    }
+}
+
+
+public struct SpeakEntity {
+    let speakId: String
+    let speakText: String
+    let speechRate: Float
+    let pitch: Float
+
+    public init(speakId: String, speakText: String, speechRate: Float = 0.5, pitch: Float = 1.0) {
+        self.speakId = speakId
+        self.speakText = speakText
+        self.speechRate = speechRate
+        self.pitch = pitch
+    }
+}
+
+
+public protocol SpeakResult {
+    func onSpeakStatus(utteranceId: String, speakStatus: SpeakStatus)
+}
+
+public enum SpeakStatus: Int {
+    case start = 1
+    case pause = 2
+    case resume = 3
+    case stop = 4
+    case done = 5
+    case playing = -100
+    case muted = -200
+    case error = -999
+
+    public static func from(value: String) -> SpeakStatus {
+        if value.lowercased() == "start" {
+            return .start
+        } else if value.lowercased() == "pause" {
+            return .pause
+        } else if value.lowercased() == "resume" {
+            return .resume
+        } else if value.lowercased() == "stop" {
+            return .stop
+        } else if value.lowercased() == "done" {
+            return .done
+        } else if value.lowercased() == "playing" {
+            return .playing
+        } else if value.lowercased() == "muted" {
+            return .muted
+        } else {
+            return .error
+        }
+    }
+}
---
+++ TextToSpeech/ViewController.swift
@@ -0,0 +1,136 @@
+//
+//  ViewController.swift
+//  exampletts
+//
+//  Created by SUNRAIN on 3/10/25.
+//
+
+import Foundation
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKScriptMessageHandler, SpeakResult {
+    
+    private var contentWebView: WKWebView? = nil
+    private lazy var speakHeandler: SpeakHandler? = { return SpeakHandler() }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let innerWebView = WKWebView(frame: .zero)
+        innerWebView.translatesAutoresizingMaskIntoConstraints = false
+        innerWebView.configuration.preferences.javaScriptCanOpenWindowsAutomatically = true
+        innerWebView.scrollView.bounces = false
+        innerWebView.scrollView.isPagingEnabled = false
+        innerWebView.scrollView.alwaysBounceVertical = false
+        innerWebView.scrollView.showsVerticalScrollIndicator = false
+        innerWebView.scrollView.showsHorizontalScrollIndicator = false
+        innerWebView.scrollView.contentInsetAdjustmentBehavior = .never
+        innerWebView.scrollView.refreshControl = UIRefreshControl()
+        // webview -> javascript
+        innerWebView.configuration.userContentController.add(self, name: "treasureComics")
+        self.view.addSubview(innerWebView)
+        NSLayoutConstraint.activate([
+            innerWebView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            innerWebView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            innerWebView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            innerWebView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        contentWebView = innerWebView
+        do {
+            let localFilePath = Bundle.main.path(forResource: "webview_content", ofType: "html")
+            let contents =  try String(contentsOfFile: localFilePath!, encoding: .utf8)
+            let baseUrl = URL(fileURLWithPath: localFilePath!)
+            contentWebView?.loadHTMLString(contents as String, baseURL: baseUrl)
+        } catch {
+            print("error: \(error)")
+        }
+        
+        if let speak = speakHeandler {
+            speak.speakStatusListener(result: self)
+        }
+        
+        if #available(iOS 16.4, *) {
+            contentWebView?.isInspectable = true
+        }
+    }
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        print("userContentController::message { name: \(message.name), body: \(message.body) }")
+        if message.name == "treasureComics", let messageBody = message.body as? String {
+            do {
+                if let withData = messageBody.data(using: .utf8) {
+                    if let json = try JSONSerialization.jsonObject(with: withData, options: .allowFragments) as? [String: AnyObject] {
+                        // make contract
+                        let request = (json["request"] as? String) ?? ""
+                        let action = (json["action"] as? String) ?? ""
+                        let params = json["parameter"] as? [String: AnyObject]
+                        if request == "postSpeak" {
+                            if action == "start" {
+                                let speakId = params?["speakId"] as? String ?? ""
+                                let speakText = params?["speakText"] as? String ?? ""
+                                let speechRate = params?["speechRate"] as? Float ?? 0.5
+                                let pitch = params?["pitch"] as? Float ?? 1.0
+                                let speakEntity = SpeakEntity(speakId: speakId, speakText: speakText, speechRate: speechRate, pitch: pitch)
+                                speakHeandler?.speak(speakEntity: speakEntity)
+                                return
+                            }
+                            
+                            if action == "stop" {
+                                speakHeandler?.speakStop()
+                                return
+                            }
+                            
+                            if action == "pause" {
+                                speakHeandler?.speakPause()
+                                return
+                            }
+                            
+                            if action == "resume" {
+                                speakHeandler?.speakResume()
+                                return
+                            }
+                        }
+                    }
+                }
+            } catch {
+                print("error: \(error)")
+            }
+        }
+    }
+ 
+    // MARK:- SpeakResult Implemetation
+    public func onSpeakStatus(utteranceId: String, speakStatus: SpeakStatus) {
+        var param: [String: Any] = [:]
+        param["speakId"] = utteranceId
+        param["speakStatus"] = speakStatus.rawValue
+        sendPostResponse(callbackName: "postSpeakCallback", params: param)
+    }
+    
+    func sendPostResponse(callbackName: String, params: [String: Any]? = nil) {
+        var paramString: String = ""
+        if params != nil {
+            guard let json = try? JSONSerialization.data(withJSONObject: params!, options: .fragmentsAllowed) else {
+                print("Something is wrong while converting dictionary to JSON data.")
+                return
+            }
+            guard let paramStringify = String(data: json, encoding: .utf8) else {
+                print("Something is wrong while converting JSON data to JSON string.")
+                return
+            }
+            paramString = paramStringify.replacingOccurrences(of: "\"", with: "\\\"")
+            paramString = "'\(paramString)'"
+        }
+        DispatchQueue.main.async {
+            self.contentWebView?.evaluateJavaScript("(function(){\(callbackName)(\(paramString));})();")
+        }
+    }
+    
+    deinit{
+        self.contentWebView?.removeFromSuperview()
+        if #available(iOS 14.0, *) {
+            self.contentWebView?.configuration.userContentController.removeAllScriptMessageHandlers()
+        }
+    }
+}
+
